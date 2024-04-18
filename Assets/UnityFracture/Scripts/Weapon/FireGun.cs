1a7bc21e2dfd3bda036cff82e22d8a19f897b210
CiscoGalvan <fragalva@ucm.es>
2024-04-18 09:39:54 +0200

    lo que termine ayer

--- Assets/UnityFracture/Scripts/Weapon/FireGun.cs
+++ Assets/UnityFracture/Scripts/Weapon/FireGun.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 namespace Project.Scripts.Weapon
@@ -5,9 +6,17 @@ namespace Project.Scripts.Weapon
     public class FireGun : MonoBehaviour
     {
         [SerializeField] private Transform barrelEnd;
-        [SerializeField] private float radius = 0.1f;
+        [SerializeField] private Transform dir;
+
+        private Vector3 shootDir;
+
+		[SerializeField]
+		private GameObject prefab;
+		[SerializeField] private float radius = 0.1f;
         [SerializeField] private float velocity = 1000f;
         [SerializeField] private float mass = .5f;
+
+        [SerializeField] private AudioSource audio;
 
         public float Radius
         {
@@ -27,31 +36,33 @@ namespace Project.Scripts.Weapon
             set { mass = value; }
         }
 
-        void Update()
+		void Update()
         {
+			shootDir = dir.position - barrelEnd.position;
             if (Input.GetMouseButtonDown(0))
             {
                 FireBullet();
             }
+            
         }
 
         private void FireBullet()
         {
-            var bullet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            
+            var bullet = GameObject.Instantiate(prefab);
             bullet.transform.position = barrelEnd.position;
-            bullet.transform.localScale = Vector3.one * Radius;
+            bullet.transform.forward = shootDir;
 
-            var mat = bullet.GetComponent<Renderer>().material;
-            mat.color = Color.red;
-            mat.EnableKeyword("_EMISSION");
-            mat.SetColor("_EmissionColor", Color.white);
+			bullet.transform.localScale = Vector3.one * Radius;
 
+           
             var rb = bullet.AddComponent<Rigidbody>();
-            rb.velocity = transform.forward * Velocity;
+            rb.velocity = bullet.transform.forward * Velocity;
             rb.mass = mass;
             rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
-            
-            Destroy(bullet, 5);
-        }
+
+            audio.Play();
+			Destroy(bullet, 3);
+		}
     }
 }
